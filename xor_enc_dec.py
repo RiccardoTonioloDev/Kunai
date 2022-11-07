0a3aa7c6ca931af9ba4903fb14a9393e45d01102
kara <ssctonioloriccardo@gmail.com>
2022-11-07 20:57:14 +0100

    xor is now fully functional (for int and str keys)
    the data could be a string, or even a single char.

--- xor_enc_dec.py
+++ xor_enc_dec.py
@@ -1,10 +1,46 @@
-# Funziona ma non esattamente.
-def xor_enc_dec(data, key):
+# THE KEY IS A STRING
+def xor_enc_strk(data, key):
+    # PRE: data is a string; key is a string
+    # POST: it's returned an array of binaries in hex form
     newKey = []
     for idx in range(len(data)):
         newKey.insert(0, key[idx % len(key)])
-    return ''.join([chr(ord(x) ^ ord(y)) for x, y in zip(data, key)])
+    return [chr(ord(x) ^ ord(y)) for x, y in zip(data, newKey)]
+
+
+def xor_dec_strk(data, key):
+    # PRE: data it's an array of binaries in hex form; key is a string
+    # POST: it's returned a decrypted string (it's the right string, only if the right
+    # key is used)
+    newKey = []
+    for idx in range(len(data)):
+        newKey.insert(0, key[idx % len(key)])
+    return ''.join([chr(ord(x) ^ ord(y)) for x, y in zip(data, newKey)])
+
+
+# THE KEY IS AN INTEGER/CHAR
+
+
+def xor_enc_intk(data, key):
+    # PRE: data is a string; key is an integer
+    # POST: it's returned an array of binaries in hex form
+    newKey = []
+    for idx in range(len(data)):
+        newKey.insert(0, key)
+    return [chr(ord(x) ^ y) for x, y in zip(data, newKey)]
+
+
+def xor_dec_intk(data, key):
+    # PRE: data it's an array of binaries in hex form; key is an integer
+    # POST: it's returned a decrypted string (it's the right string, only if the right
+    # key is used)
+    newKey = []
+    for idx in range(len(data)):
+        newKey.insert(0, key)
+    return ''.join([chr(ord(x) ^ y) for x, y in zip(data, newKey)])
 
 
 # USAGE
-print(xor_enc_dec(xor_enc_dec("ciao menny", "hello"), "hello"))
+print(xor_dec_strk(xor_enc_strk('c', "un carattere"), "un carattere"))
+print(xor_dec_strk(xor_enc_strk("ciao menny", "hello"), "hello"))
+print(xor_dec_intk(xor_enc_intk("ciao carra", 150), 150))
