a473f91a2369a563767808229207e86295e9eccc
kara <ssctonioloriccardo@gmail.com>
2022-11-19 18:23:21 +0100

    Improved the logic to avoid "repeting solution" errors.

--- char_replacer.py
+++ char_replacer.py
@@ -1,8 +1,16 @@
 def replace_char_by_list(text, tuplesForSubstituitions):
-    for tupl in tuplesForSubstituitions:
-        # for each tuple of substitution it replaces the first element with the second
-        # inside of text
-        text = text.replace(tupl[0], tupl[1])
+    array_of_switched_chars = []
+    for idx, tupl in enumerate(tuplesForSubstituitions):
+        # Here it makes sure that we are replacing different chars with the same solution
+        if (tupl[1] not in array_of_switched_chars):
+            # for each tuple of substitution it replaces the first element with the second
+            # inside of text
+            text = text.replace(tupl[0], tupl[1])
+            array_of_switched_chars.append(tupl[1])
+        else:
+            print(
+                'WARNING: you are trying to replace more than one char with: ', tupl[1])
+
     return text
 
 
