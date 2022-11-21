4a6872a726179345b96634ccb2862b68cc5ace99
Menny <giovanni.menon15@gmail.com>
2022-11-21 17:59:47 +0100

    Update count_words.py

--- count_words.py
+++ count_words.py
@@ -1,22 +1,24 @@
 import string
 
 
-def count_words(text, letters):
-    Words = {}
-
+def count_words(text, letters = None):
     for letter in text:
-        if letter not in string.ascii_letters:
-            text = text.replace(letter, " ")
+            if letter not in string.ascii_letters:
+                text = text.replace(letter, " ")
 
-    for word in text.split():
-        if word in Words:
-            Words[word] += 1
-        elif len(word) == letters:
-            Words[word] = 1
-
-    Words = dict(sorted(Words.items(), key=lambda item: item[1], reverse=True))
-    return Words
+    if letters is None:
+        Words = text.split()
+        wfreq=[Words.count(w) for w in Words]
+        return dict(sorted(dict(zip(Words, wfreq)).items() , key=lambda item:item[1], reverse=True))
+    else:
+        Words = {}
+        for word in text.split():
+            if word in Words:
+                Words[word] += 1
+            elif len(word) == letters:
+                Words[word] = 1
+        return dict(sorted(Words.items(), key=lambda item: item[1], reverse=True))
 
 
 # USAGE
-print(count_words("pronto prova, sa sa, prova microfono, uno due 3", 3))
+print(count_words("pronto prova, sa sa, prova microfono, uno due 3"))
